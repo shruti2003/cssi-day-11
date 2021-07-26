53b61a5cc735e1e59864374908e79d577ea487e6
Shruti <shruti.p2003@gmail.com>
2021-07-26 18:29:56 +0000

    write notes to database

---
+++ public/js/writeNote.js
@@ -0,0 +1,39 @@
+let googleUser = null
+
+//when the window has fully loaded run this function
+window.onload = () => {
+    //when the authorization state of the page changes
+    firebase.auth().onAuthStateChanged(user =>{
+        if(user){
+            //if user has logged in 
+            console.log("logged in as", user.displayName)
+            googleUser = user
+        } else {
+            //user has not logged in
+            console.log("not logged in")
+        }
+    })
+
+    const createNoteButton = document.querySelector("#createNoteButton")
+    
+    //get values from the form
+    createNoteButton.addEventListener("click", () => {
+        console.log("clicked")
+        const noteTitle = document.querySelector("#noteTitle").value
+        const noteText = document.querySelector("#noteText").value
+        console.log(noteTitle, noteText)
+
+        //write these values to the database
+        firebase.database().ref(`/users/${googleUser.uid}`).push({
+            title: noteTitle, 
+            text: noteText
+        }).then(() => {
+            console.log("database write succesful")
+            document.querySelector("#noteTitle").value = ""
+            document.querySelector("#noteText").value = ""            
+        })
+        .catch(error => {
+            console.log("error writing a new note", error)
+        })
+    })
+}
