17bb904f4b5261e86d6a86353599bbad5b1f8d27
Shruti <shruti.p2003@gmail.com>
2021-07-26 17:42:59 +0000

    Rediret working.

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -2,6 +2,20 @@ console.log("signIn.js loaded")
 
 const signInButton = document.querySelector(".button");
 
+//event listener for sign in button
 signInButton.addEventListener("click", () => {
     console.log("clicked!")
+    //authentication
+    const provider = new firebase.auth.GoogleAuthProvider()
+    //popup google authentication
+    firebase.auth().signInWithPopup(provider).then(result => {
+        //log the user that is logged in
+        const user = result.user
+        console.log("login success", user)
+        window.location = "writeNote.html"
+    })
+   //error handling
+   .catch(error => {
+       console.log("login failed", error)
+   })
 })
