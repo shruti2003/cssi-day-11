9c8d0c62eba979be07f77afac7feed392ed3f9a3
Shruti <shruti.p2003@gmail.com>
2021-07-26 18:55:12 +0000

    mild goals.

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -2,12 +2,16 @@ let googleUser = null
 
 //when the window has fully loaded run this function
 window.onload = () => {
+    //welcome the user
+    
     //when the authorization state of the page changes
     firebase.auth().onAuthStateChanged(user =>{
         if(user){
             //if user has logged in 
             console.log("logged in as", user.displayName)
             googleUser = user
+            const welcome = document.querySelector("#welcome")
+            welcome.innerHTML = `Welcome ${user.displayName}!`
         } else {
             //user has not logged in
             console.log("not logged in")
@@ -21,16 +25,18 @@ window.onload = () => {
         console.log("clicked")
         const noteTitle = document.querySelector("#noteTitle").value
         const noteText = document.querySelector("#noteText").value
-        console.log(noteTitle, noteText)
+        const noteCreated = Date()
+        console.log(noteTitle, noteText, noteCreated)
 
         //write these values to the database
         firebase.database().ref(`/users/${googleUser.uid}`).push({
             title: noteTitle, 
-            text: noteText
+            text: noteText,
+            created: noteCreated
         }).then(() => {
             console.log("database write succesful")
             document.querySelector("#noteTitle").value = ""
-            document.querySelector("#noteText").value = ""            
+            document.querySelector("#noteText").value = ""
         })
         .catch(error => {
             console.log("error writing a new note", error)
